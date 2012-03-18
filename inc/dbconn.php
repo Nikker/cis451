aae548fac0894cb74d288f82382311c628d95c57
Nicole DelRosso <nikker@playa.cs.uoregon.edu>
2012-03-17 22:05:32 -0700

    Fixed db connection values

--- inc/dbconn.php
+++ inc/dbconn.php
@@ -1,12 +1,13 @@
 <?php
-
 $db_cnf = @parse_ini_file(BETA_MYCNF_DIR . '/.my.cnf');
+
 $db = new mysqli(
-  $db_cnf['client']['host'], 
+  $db_cnf['host'], 
   'beta',
   'holy password dude',
   'cis451_final',
-  $db_cnf['client']['port']
+  $db_cnf['port'],
+  $db_cnf['socket']
 ) or die('Could not connect to database');
 
 ?>
