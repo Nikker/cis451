a9e4922ff0d6efb43ef4ea0d88b2e90719c87c52
Nikki DelRosso <nikker@cs.uoregon.edu>
2012-03-21 00:08:07 -0700

    Modified title for user list to include page number only on pages that aren't the first page

--- user/list.php
+++ user/list.php
@@ -4,7 +4,7 @@
 $count = $db->query("SELECT COUNT(*) as `count` FROM `user`;")->fetch_object()->count;
 
 $tpl->page = array_key_exists('page', $_GET)? $_GET['page'] : 1;
-$per_page = 50;
+$per_page = 20;
 $start = ($tpl->page - 1) * $per_page;
 $tpl->pages = ceil($count/$per_page);
 
@@ -17,7 +17,8 @@ while ($user = $users->fetch_object()) {
   $tpl->users[] = $user;
 }
 
-$tpl->title = sprintf("User list page %d",$tpl->page);
+$tpl->title = "User list";
+if ($tpl->page > 1) $tpl->title .= "- page {$tpl->page}";
 $tpl->content = $tpl->fetch('user/list.tpl.php');
 $tpl->display();
 
