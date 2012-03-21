357c974a713ec82bb1fed4edd1ab0af3a416d995
Nikki DelRosso <nikker@cs.uoregon.edu>
2012-03-21 00:08:35 -0700

    Added a 'no users to display' message to the user list page.

--- views/user/list.tpl.php
+++ views/user/list.tpl.php
@@ -1,8 +1,12 @@
 <h1>User List</h1>
-<?php $this->display('paging.tpl.php'); ?>
-<ul>
-  <?php foreach ($this->users as $user): ?>
-    <li><a href="<?php $this->eprint($user->username); ?>"><?php $this->eprint($user->username); ?></a></li>
-  <?php endforeach; ?>
-</ul>
-<?php $this->display('paging.tpl.php'); ?>
+<?php if (empty($this->users)): ?>
+  <p>There are no users to display.</p>
+<?php else: ?>
+  <?php $this->display('paging.tpl.php'); ?>
+  <ul>
+    <?php foreach ($this->users as $user): ?>
+      <li><a href="<?php $this->eprint($user->username); ?>"><?php $this->eprint($user->username); ?></a></li>
+    <?php endforeach; ?>
+  </ul>
+  <?php $this->display('paging.tpl.php'); ?>
+<?php endif; ?>
